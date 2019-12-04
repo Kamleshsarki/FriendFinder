a3ab5b0ed0a0cd2796f2f7f368fec54c13d353fc
Kamlesh <kamlesh.sarki@gmail.com>
2019-12-03 18:52:03 -0800

    server.js

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+    {
+name : "Tom Hardey",
+photo :" https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.lookslikefilm.com%2Fwp-content%2Fuploads%2F2019%2F09%2FChristinEidePhotography_VSCO_www.christineide.com_.jpg&imgrefurl=https%3A%2F%2Fwww.lookslikefilm.com%2F2019%2F09%2F20%2Felopement-pictures%2F&docid=6hfX7J0n9wdW2M&tbnid=yzO4ug3pTi8xTM%3A&vet=10ahUKEwju35e60pjmAhXiN30KHevLAp4QMwiIASgQMBA..i&w=900&h=600&safe=active&bih=657&biw=1366&q=pictures&ved=0ahUKEwju35e60pjmAhXiN30KHevLAp4QMwiIASgQMBA&iact=mrc&uact=8",
+scores : [5,1,4,4,5,1,2,5,4,4,]
+
+}
+
+]
